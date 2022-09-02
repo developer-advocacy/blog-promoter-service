b712172164834c12e8094354e56453e98765dd9a
Josh Long <josh@joshlong.com>
2022-09-02 16:46:55 -0700

    new breaking changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,7 +9,7 @@ export IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 
 docker rmi -f $IMAGE_NAME
 cd $ROOT_DIR
-./mvnw -DskipTests=true spring-javaformat:apply clean deploy spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+./mvnw -U -DskipTests=true spring-javaformat:apply clean deploy spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
 docker push $IMAGE_NAME
 
 echo $IMAGE_NAME
